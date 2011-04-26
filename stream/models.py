0312685f2253ca061b2ea3565b5b4ff573730352
Jason Moiron <jmoiron@jmoiron.net>
2011-04-26 01:47:38 -0400

    add paginated view of stream plugins, though it's really very crappy;  needed it to test out bitbucket stuff

--- stream/models.py
+++ stream/models.py
@@ -6,6 +6,8 @@
 from flask import render_template
 from micromongo import *
 
+__all__ = ['Entry', 'Plugin']
+
 class Entry(Model):
     collection = 'jmoiron.stream_entry'
     spec = {
@@ -25,4 +27,13 @@ class Entry(Model):
         return  render_template(template, entry=self)
 
 
+class Plugin(Model):
+    collection = 'jmoiron.stream_plugin'
+    spec = {
+        'tag' : Field(required=True, type=basestring),
+        'name' : Field(required=True, type=basestring),
+        'id' : Field(type=int), # legacy
+        'interval' : Field(type=int),
+        'arguments': Field(required=True)
+    }
 
--- stream/views.py
+++ stream/views.py
@@ -3,12 +3,32 @@
 
 """stream application views"""
 
-from flask import Module, g, request, render_template
+from flask import *
 from models import Entry
 
 stream = Module(__name__, 'stream')
 
+per_page = 25
+
 @stream.route("/")
 def index():
-    entries = Entry.find().order_by('-timestamp')[:25]
+    entries = Entry.find().order_by('-timestamp')[:per_page]
     return render_template('stream/index.html', **locals())
+
+@stream.route("/page/<int:num>")
+def show_page(num):
+    total = Entry.find().count()
+    begin = (num-1) * per_page
+    end = num * per_page
+    if begin > total:
+        abort(404)
+    entries = Entry.find().order_by('-timestamp')[begin:end]
+    has_next = end < total
+    has_prev = num > 1
+    return render_template('stream/index.html', **{
+        'entries': entries,
+        'has_next': has_next,
+        'has_prev': has_prev,
+        'current': num
+    })
+
