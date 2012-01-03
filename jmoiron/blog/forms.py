5577f15be8a99a92b657de690ad0978f8a068cb8
Jason Moiron <jmoiron@jmoiron.net>
2012-01-03 18:01:03 -0500

    world-breaking reorganization

---
+++ jmoiron/blog/forms.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Forms for admin blog post CRUD."""
+
+from wtforms import *
+
+class PostForm(Form):
+    title = TextField('Title', [validators.required])
+    is_published = BooleanField('Publish?')
+    timestamp = TextField('Timestamp')
+    body = TextAreaField('Body', [validators.required])
+    tags = SelectMultipleField('Tags')
+    summary = TextAreaField('Summary')
+
+
