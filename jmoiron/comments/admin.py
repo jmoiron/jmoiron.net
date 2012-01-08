574897d47c21f0f65df7a0c7678043619d798b07
Jason Moiron <jmoiron@jmoiron.net>
2012-01-08 16:12:12 -0500

    some admin appearance fixes and comments additions

--- jmoiron/comments/admin.py
+++ jmoiron/comments/admin.py
@@ -15,12 +15,16 @@ admin = Manager(blueprint)
 def summary():
     """Display a summary table of posts as the default dashboard view of the
     blog admin."""
-    return comment_summary(count=7)
+    return comment_list(count=7)
 
 comment = Module("comment")
 
 @comment.register("summary")
-def comment_summary(count=10):
+def comment_summary():
+    return comment_list(count=10)
+
+@comment.register("list")
+def comment_list(count=25):
     comments = Comment.find().order_by("-timestamp")[:count]
     module = comment
     return render_template("comments/admin/comment_summary.html", **locals())
