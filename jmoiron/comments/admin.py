c3784fffe0e61f4504c87a3cb4b18421b0f78c47
Jason Moiron <jmoiron@jmoiron.net>
2012-03-04 11:14:51 -0500

    implement a new widget system based on macros for admin rather than rigid template inheritance;  removed a lot of duplication in client code

--- jmoiron/comments/admin.py
+++ jmoiron/comments/admin.py
@@ -5,6 +5,7 @@
 
 from flask import *
 from flaskext.wtf import *
+from jmoiron.utils import Page
 from jmoiron.admin.models import Manager, Module
 
 from models import *
@@ -15,19 +16,22 @@ admin = Manager(blueprint)
 def summary():
     """Display a summary table of posts as the default dashboard view of the
     blog admin."""
-    return comment_list(count=7)
+    return comment_summary(count=5)
 
 comment = Module("comment")
 
 @comment.register("summary")
-def comment_summary():
-    return comment_list(count=10)
-
-@comment.register("list")
-def comment_list(count=25):
+def comment_summary(count=10):
     comments = Comment.find().order_by("-timestamp")[:count]
     module = comment
     return render_template("comments/admin/comment_summary.html", **locals())
+
+@comment.register("list")
+def comment_list(count=25):
+    page = Page(int(request.args.get('page', 1)), count, Comment.find().order_by("-timestamp").count())
+    comments = Comment.find().order_by("-timestamp")[page.slice()]
+    module = comment
+    return render_template("comments/admin/comment_list.html", **locals())
 
 
 admin.add_module(comment)
