435d3d66a3e854b90196aadfccb23814e5517152
Jason Moiron <jmoiron@jmoiron.net>
2012-03-04 12:15:44 -0500

    update edit form for bootstrap 2

--- config.py
+++ config.py
@@ -9,10 +9,12 @@ class Config(object):
     DATABASE_URI = "mongodb://localhost:27017/"
     DATABASE_NAME = "jmoiron"
     SECRET_KEY = '\x10t\x98\xaeR:0\xc2\xea\x8frl b=\xde'
+    USE_LESS = True
 
 class DevelopmentConfig(Config):
     DEBUG = True
     OFFLINE_MODE = True
+    #USE_LESS = False
 
 class ProductionConfig(Config):
     pass
